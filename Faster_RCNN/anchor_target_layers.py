ee52b4ddc04f36bb22177fb5ca05b18a4406ff9c
BlainWu <896511142@qq.com>
2020-11-04 16:30:48 +0800

    update

---
+++ Faster_RCNN/anchor_target_layers.py
@@ -0,0 +1,7 @@
+# ------------------------------------------------
+# Project: object-detection
+# Author:Peilin Wu - Northumbria University
+# File name :anchor_target_layers.py
+# Created time :2020/11
+# ------------------------------------------------
+from .generate_anchors import generate_anchors
